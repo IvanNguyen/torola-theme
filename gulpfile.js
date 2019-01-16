e2b4bed5df9e3b1a013266068a34a8f373979244
Dat NGUYEN <datle@itc-admin-mm-015s-Mac-mini.local>
2019-01-16 15:40:41 +0700

    Setup project's file structure

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('serve', ['css'], function(){
         server: "./"
     });
 
-    gulp.watch("./styles/css/*.css", ['css']).on('change', browserSync.reload);
+    gulp.watch("./css/*.css", ['css']).on('change', browserSync.reload);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
@@ -39,7 +39,7 @@ gulp.task('css', function(){
         nestedProps()
         
     ];
-    return gulp.src('./styles/css/*.css')
+    return gulp.src('./css/*.css')
         .pipe(postcss(plugins))
         .pipe(gulp.dest('./dest'));
 });
